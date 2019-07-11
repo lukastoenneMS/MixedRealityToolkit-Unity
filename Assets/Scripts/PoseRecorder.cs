44e6994a5c57f0eca05cda2f91264b99aa51d010
Lukas Tönne <lutnne@microsoft.com>
2019-07-11 20:39:48 +0100

    Start and stop tracking when hand controllers are tracking.

--- Assets/Scripts/PoseRecorder.cs
+++ Assets/Scripts/PoseRecorder.cs
@@ -5,6 +5,7 @@ using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 using Pose = Microsoft.MixedReality.Toolkit.Utilities.MixedRealityPose;
@@ -13,49 +14,80 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
 {
     public class PoseRecorder : InputSystemGlobalHandlerListener, IMixedRealitySourceStateHandler, IMixedRealityHandJointHandler
     {
-        public PoseConfiguration config { get; private set; }
+        public GameObject JointIndicatorPrefab;
 
-        public bool IsTracking => sourceId > 0;
-
-        private uint sourceId = 0;
+        public PoseConfiguration PoseConfig { get; private set; }
+        public Handedness PoseHandedness { get; private set; }
 
         private readonly Dictionary<TrackedHandJoint, GameObject> jointIndicators = new Dictionary<TrackedHandJoint, GameObject>();
 
-        private static readonly TrackedHandJoint[] jointValues = (TrackedHandJoint[])Enum.GetValues(typeof(TrackedHandJoint));
+        private static readonly TrackedHandJoint[] jointValues = new TrackedHandJoint[]
+        {
+            // TrackedHandJoint.None,
+            // TrackedHandJoint.Wrist,
+            // TrackedHandJoint.Palm,
+            TrackedHandJoint.ThumbMetacarpalJoint,
+            TrackedHandJoint.ThumbProximalJoint,
+            TrackedHandJoint.ThumbDistalJoint,
+            TrackedHandJoint.ThumbTip,
+            TrackedHandJoint.IndexMetacarpal,
+            TrackedHandJoint.IndexKnuckle,
+            TrackedHandJoint.IndexMiddleJoint,
+            TrackedHandJoint.IndexDistalJoint,
+            TrackedHandJoint.IndexTip,
+            TrackedHandJoint.MiddleMetacarpal,
+            TrackedHandJoint.MiddleKnuckle,
+            TrackedHandJoint.MiddleMiddleJoint,
+            TrackedHandJoint.MiddleDistalJoint,
+            TrackedHandJoint.MiddleTip,
+            TrackedHandJoint.RingMetacarpal,
+            TrackedHandJoint.RingKnuckle,
+            TrackedHandJoint.RingMiddleJoint,
+            TrackedHandJoint.RingDistalJoint,
+            TrackedHandJoint.RingTip,
+            TrackedHandJoint.PinkyMetacarpal,
+            TrackedHandJoint.PinkyKnuckle,
+            TrackedHandJoint.PinkyMiddleJoint,
+            TrackedHandJoint.PinkyDistalJoint,
+            TrackedHandJoint.PinkyTip,
+        };
+
+        private readonly List<IMixedRealityHand> trackedHands = new List<IMixedRealityHand>();
 
         void Awake()
         {
-            foreach (TrackedHandJoint joint in jointValues)
+            if (JointIndicatorPrefab)
             {
-                jointIndicators = 
+                foreach (TrackedHandJoint joint in jointValues)
+                {
+                    var jointOb = GameObject.Instantiate(JointIndicatorPrefab, transform);
+                    jointIndicators.Add(joint, jointOb);
+                    jointOb.SetActive(false);
+                }
             }
         }
 
-        /// <inheritdoc />
         public void OnSourceDetected(SourceStateEventData eventData)
         {
             var hand = eventData.Controller as IMixedRealityHand;
             if (hand != null)
             {
-                TryStartTracking(hand);
+                StartTracking(hand);
             }
         }
 
-        /// <inheritdoc />
         public void OnSourceLost(SourceStateEventData eventData)
         {
-            if (eventData.SourceId == sourceId)
+            var hand = eventData.Controller as IMixedRealityHand;
+            if (hand != null)
             {
-                StopTracking();
+                StopTracking(hand);
             }
         }
 
         public void OnHandJointsUpdated(InputEventData<IDictionary<TrackedHandJoint, Pose>> eventData)
         {
-            if (eventData.SourceId == sourceId)
-            {
-                UpdateHandPose(eventData.InputData);
-            }
+            // UpdateHandPose(eventData.InputData);
         }
 
         public new void OnDisable()
@@ -64,25 +96,54 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
             StopTracking();
         }
 
-        private bool TryStartTracking(IMixedRealityHand hand)
+        private void StartTracking(IMixedRealityHand hand)
         {
-            if (sourceId == 0)
+            if (!trackedHands.Any(h => h.ControllerHandedness == hand.ControllerHandedness))
             {
-                sourceId = hand.InputSource.SourceId;
-                config = new PoseConfiguration();
+                trackedHands.Add(hand);
 
                 var handPose = PollHandPose(hand);
                 UpdateHandPose(handPose);
-
-                return true;
             }
-            return false;
         }
 
         private void StopTracking()
         {
-            sourceId = 0;
-            config = null;
+            trackedHands.Clear();
+
+            ClearHandPose();
+        }
+
+        private void StopTracking(IMixedRealityHand hand)
+        {
+            trackedHands.RemoveAll(h => h == hand);
+
+            ClearHandPose();
+        }
+
+        private void StopTrackingAll(Predicate<IMixedRealityHand> pred)
+        {
+            trackedHands.RemoveAll(pred);
+
+            ClearHandPose();
+        }
+
+        private void InitPoseConfig()
+        {
+            var hand = trackedHands.Last();
+            if (hand != null)
+            {
+                PoseHandedness = hand.ControllerHandedness;
+
+                PoseConfig = new PoseConfiguration();
+                PoseConfig.Init(PollHandPose(hand).Select(item => item.Value.Position));
+            }
+        }
+
+        private void DiscardPoseConfig()
+        {
+            PoseConfig = null;
+            PoseHandedness = Handedness.None;
         }
 
         private static IDictionary<TrackedHandJoint, Pose> PollHandPose(IMixedRealityHand hand)
@@ -100,17 +161,43 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
 
         private void UpdateHandPose(IDictionary<TrackedHandJoint, Pose> joints)
         {
-
+            if (PoseConfig != null)
+            {
+                foreach (var item in joints)
+                {
+                    if (jointIndicators.TryGetValue(item.Key, out GameObject jointOb))
+                    {
+                        jointOb.SetActive(true);
+                        jointOb.transform.position = item.Value.Position;
+                        jointOb.transform.rotation = item.Value.Rotation;
+                    }
+                    else
+                    {
+                        jointOb.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                ClearHandPose();
+            }
         }
 
-        /// <inheritdoc />
+        private void ClearHandPose()
+        {
+            foreach (var item in jointIndicators)
+            {
+                var jointOb = item.Value;
+                jointOb.SetActive(false);
+            }
+        }
+
         protected override void RegisterHandlers()
         {
             InputSystem?.RegisterHandler<IMixedRealitySourceStateHandler>(this);
             InputSystem?.RegisterHandler<IMixedRealityHandJointHandler>(this);
         }
 
-        /// <inheritdoc />
         protected override void UnregisterHandlers()
         {
             InputSystem?.UnregisterHandler<IMixedRealitySourceStateHandler>(this);
