a18ca0e76c77c974e622ff110d7a052d2c822aad
Lukas Tönne <lutnne@microsoft.com>
2019-07-19 08:31:53 +0100

    More test values for Givens rotation approximation.

--- Assets/Tests/MathUtilsTests.cs
+++ Assets/Tests/MathUtilsTests.cs
@@ -34,38 +34,56 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
         [Test]
         public void ApproximateGivensRotation()
         {
-            float[][] values = new float[][]
+            float[] values = new float[]
             {
-                new float[] { 1.0f, 0.0f, 1.0f },
+                1.0f, 2.0f, 3.0f, 4.0f,
+                0.1f, 0.01f, 0.001f, 1.0e-12f,
+                10.0f, 100.0f, 1000.0f, 1.0e12f,
+                (float)System.Math.PI, (float)System.Math.E, 
             };
 
-            foreach (float[] v in values)
+            foreach (float a11 in values)
             {
-                float a11 = v[0];
-                float a12 = v[1];
-                float a21 = a12;
-                float a22 = v[2];
-
-                MathUtils.ApproximateGivensRotation(a11, a12, a21, out float c, out float s);
-                Assert.LessOrEqual(c, 1.0f);
-                Assert.GreaterOrEqual(c, -1.0f);
-                Assert.LessOrEqual(s, 1.0f);
-                Assert.GreaterOrEqual(s, -1.0f);
-                Assert.AreEqual(1.0f, c*c + s*s, ExpectedGivensEpsilon);
-
-                float aa11 =  a11 * c + a12 * s;
-                float aa12 = -a11 * s + a12 * c;
-                float aa21 =  a21 * c + a22 * s;
-                float aa22 = -a21 * s + a22 * c;
-
-                float aaa11 = c * aa11 - s * aa21;
-                float aaa12 = c * aa12 - s * aa22;
-                float aaa21 = s * aa11 + c * aa21;
-                float aaa22 = s * aa12 + c * aa22;
-
-                Assert.AreEqual(0.0f, aaa12, ExpectedGivensEpsilon, $"Givens rotation for (({v[0]}, {v[1]}), ({v[1]}, {v[2]})) does not diagonalize matrix");
-                Assert.AreEqual(0.0f, aaa21, ExpectedGivensEpsilon, $"Givens rotation for (({v[0]}, {v[1]}), ({v[1]}, {v[2]})) does not diagonalize matrix");
+                foreach (float a12 in values)
+                {
+                    foreach (float a22 in values)
+                    {
+                        TestGivensApproximation( a11,  a12,  a22);
+                        TestGivensApproximation(-a11,  a12,  a22);
+                        TestGivensApproximation( a11, -a12,  a22);
+                        TestGivensApproximation(-a11, -a12,  a22);
+                        TestGivensApproximation( a11,  a12, -a22);
+                        TestGivensApproximation(-a11,  a12, -a22);
+                        TestGivensApproximation( a11, -a12, -a22);
+                        TestGivensApproximation(-a11, -a12, -a22);
+                    }
+                }
             }
+        }
+
+        void TestGivensApproximation(float a11, float a12, float a22)
+        {
+            float a21 = a12;
+
+            MathUtils.ApproximateGivensRotation(a11, a12, a21, out float c, out float s);
+            Assert.LessOrEqual(c, 1.0f);
+            Assert.GreaterOrEqual(c, -1.0f);
+            Assert.LessOrEqual(s, 1.0f);
+            Assert.GreaterOrEqual(s, -1.0f);
+            Assert.AreEqual(1.0f, c*c + s*s, ExpectedGivensEpsilon);
+
+            float aa11 =  a11 * c + a12 * s;
+            float aa12 = -a11 * s + a12 * c;
+            float aa21 =  a21 * c + a22 * s;
+            float aa22 = -a21 * s + a22 * c;
+
+            float aaa11 = c * aa11 - s * aa21;
+            float aaa12 = c * aa12 - s * aa22;
+            float aaa21 = s * aa11 + c * aa21;
+            float aaa22 = s * aa12 + c * aa22;
+
+            Assert.AreEqual(0.0f, aaa12, ExpectedGivensEpsilon, $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
+            Assert.AreEqual(0.0f, aaa21, ExpectedGivensEpsilon, $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
         }
     }
 }
