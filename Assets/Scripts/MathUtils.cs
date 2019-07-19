07a7c51910ba8fa0114d3d6b9d38d2d82738385e
Lukas Tönne <lutnne@microsoft.com>
2019-07-19 11:48:55 +0100

    Add quaternion version of the Givens rotation approximation.

--- Assets/Scripts/MathUtils.cs
+++ Assets/Scripts/MathUtils.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.MixedReality.Toolkit.PoseMatching
@@ -35,6 +36,7 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
         /// <summary>
         /// Compute an approximate Givens rotation on a 2x2 symmetric matrix A.
         /// </summary>
+        /// <returns>The approximated Givens rotation as cosine and sine of the Givens angle</returns>
         /// <remarks>
         /// Let `A = [[a11, a12], [a12, a22]]` be a symmetric 2x2 matrix.
         /// Then the Givens rotation diagonalizes A by finding a rotation matrix `Q = [[c, -s], [s, c]]`
@@ -47,16 +49,51 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
         /// and elementary floating point operations.
         /// University of Wisconsin-Madison Department of Computer Sciences, 2011.
         /// </remarks>
-        public static void ApproximateGivensRotation(float a11, float a12, float a22, out float c, out float s)
+        public static void ApproximateGivensRotationCosSin(float a11, float a12, float a22, out float c, out float s)
         {
             float p = a12;
             float pp = p * p;
             float q = a11 - a22;
             float qq = q * q;
             bool b = pp < qq;
-            float w = OneOverSqrt(pp + qq);
-            s = b ? w * p : oneOverSqrtTwo;
-            c = b ? w * q : oneOverSqrtTwo;
+            float omega = OneOverSqrt(pp + qq);
+            s = b ? omega * p : oneOverSqrtTwo;
+            c = b ? omega * q : oneOverSqrtTwo;
+        }
+
+        // 3 + 2*sqrt(2)
+        private const float GivensApproxGamma = 5.8284271247461900976033774484194f;
+        // cos(pi/8)
+        private const float GivensApproxCosPi8 = 0.99997651217454865478849954406816f;
+        // sin(pi/8)
+        private const float GivensApproxSinPi8 = 0.00685383828411102723918684180104f;
+
+        /// <summary>
+        /// Compute an approximate Givens rotation on a 2x2 symmetric matrix A.
+        /// </summary>
+        /// <returns>The approximated Givens rotation as a quaternion</returns>
+        /// <remarks>
+        /// Let `A = [[a11, a12], [a12, a22]]` be a symmetric 2x2 matrix.
+        /// Then the Givens rotation diagonalizes A by finding a rotation matrix `Q = [[c, -s], [s, c]]`
+        /// such that `B = Q^T * A * Q` is a diagonal matrix.
+        ///
+        /// This method computes approximate cosine and sine factors, based on:
+        ///
+        /// McAdams, Aleka, et al.
+        /// Computing the singular value decomposition of 3x3 matrices with minimal branching
+        /// and elementary floating point operations.
+        /// University of Wisconsin-Madison Department of Computer Sciences, 2011.
+        /// </remarks>
+        public static void ApproximateGivensRotationQuaternion(float a11, float a12, float a22, out float x, out float w)
+        {
+            float ch = 2.0f * (a11 - a22);
+            float cch = ch * ch;
+            float sh = a12;
+            float ssh = sh * sh;
+            bool b = GivensApproxGamma * ssh < cch;
+            float omega = OneOverSqrt(cch + ssh);
+            x = b ? omega * ch : GivensApproxCosPi8;
+            w = b ? omega * sh : GivensApproxSinPi8;
         }
     }
 }
--- Assets/Tests/MathUtilsTests.cs
+++ Assets/Tests/MathUtilsTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using NUnit.Framework;
+using System;
+using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.PoseMatching
 {
@@ -17,12 +19,12 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
                 1.0f, 2.0f, 3.0f, 4.0f,
                 0.1f, 0.01f, 0.001f, 1.0e-12f,
                 10.0f, 100.0f, 1000.0f, 1.0e12f,
-                (float)System.Math.PI, (float)System.Math.E, 
+                (float)Math.PI, (float)Math.E, 
             };
 
             foreach (float v in values)
             {
-                float expected = (float)(1.0 / System.Math.Sqrt(v));
+                float expected = (float)(1.0 / Math.Sqrt(v));
                 float actual = MathUtils.OneOverSqrt(v);
                 float delta = ExpectedInvSqrtAccuracy * expected;
                 Assert.AreEqual(expected, actual, delta, $"1/sqrt({v}): Expected value is {expected}, actual value is {actual}");
@@ -39,7 +41,7 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
                 1.0f, 2.0f, 3.0f, 4.0f,
                 0.1f, 0.01f, 0.001f, 1.0e-12f,
                 10.0f, 100.0f, 1000.0f, 1.0e12f,
-                (float)System.Math.PI, (float)System.Math.E, 
+                (float)Math.PI, (float)Math.E, 
             };
 
             foreach (float a11 in values)
@@ -48,42 +50,70 @@ namespace Microsoft.MixedReality.Toolkit.PoseMatching
                 {
                     foreach (float a22 in values)
                     {
-                        TestGivensApproximation( a11,  a12,  a22);
-                        TestGivensApproximation(-a11,  a12,  a22);
-                        TestGivensApproximation( a11, -a12,  a22);
-                        TestGivensApproximation(-a11, -a12,  a22);
-                        TestGivensApproximation( a11,  a12, -a22);
-                        TestGivensApproximation(-a11,  a12, -a22);
-                        TestGivensApproximation( a11, -a12, -a22);
-                        TestGivensApproximation(-a11, -a12, -a22);
+                        TestGivensApproximationCosSin( a11,  a12,  a22);
+                        TestGivensApproximationCosSin(-a11,  a12,  a22);
+                        TestGivensApproximationCosSin( a11, -a12,  a22);
+                        TestGivensApproximationCosSin(-a11, -a12,  a22);
+                        TestGivensApproximationCosSin( a11,  a12, -a22);
+                        TestGivensApproximationCosSin(-a11,  a12, -a22);
+                        TestGivensApproximationCosSin( a11, -a12, -a22);
+                        TestGivensApproximationCosSin(-a11, -a12, -a22);
+
+                        TestGivensApproximationQuaternion( a11,  a12,  a22);
+                        TestGivensApproximationQuaternion(-a11,  a12,  a22);
+                        TestGivensApproximationQuaternion( a11, -a12,  a22);
+                        TestGivensApproximationQuaternion(-a11, -a12,  a22);
+                        TestGivensApproximationQuaternion( a11,  a12, -a22);
+                        TestGivensApproximationQuaternion(-a11,  a12, -a22);
+                        TestGivensApproximationQuaternion( a11, -a12, -a22);
+                        TestGivensApproximationQuaternion(-a11, -a12, -a22);
                     }
                 }
             }
         }
 
-        void TestGivensApproximation(float a11, float a12, float a22)
+        void TestGivensApproximationCosSin(float a11, float a12, float a22)
         {
-            float a21 = a12;
-
-            MathUtils.ApproximateGivensRotation(a11, a12, a21, out float c, out float s);
+            MathUtils.ApproximateGivensRotationCosSin(a11, a12, a22, out float c, out float s);
             Assert.LessOrEqual(c, 1.0f);
             Assert.GreaterOrEqual(c, -1.0f);
             Assert.LessOrEqual(s, 1.0f);
             Assert.GreaterOrEqual(s, -1.0f);
             Assert.AreEqual(1.0f, c*c + s*s, ExpectedGivensEpsilon);
 
+            float a21 = a12;
+
             float aa11 =  a11 * c + a12 * s;
             float aa12 = -a11 * s + a12 * c;
             float aa21 =  a21 * c + a22 * s;
             float aa22 = -a21 * s + a22 * c;
 
-            float aaa11 = c * aa11 - s * aa21;
-            float aaa12 = c * aa12 - s * aa22;
-            float aaa21 = s * aa11 + c * aa21;
-            float aaa22 = s * aa12 + c * aa22;
+            float aaa11 =  c * aa11 + s * aa21;
+            float aaa12 =  c * aa12 + s * aa22;
+            float aaa21 = -s * aa11 + c * aa21;
+            float aaa22 = -s * aa12 + c * aa22;
 
-            Assert.AreEqual(0.0f, aaa12, ExpectedGivensEpsilon, $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
-            Assert.AreEqual(0.0f, aaa21, ExpectedGivensEpsilon, $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
+            Assert.LessOrEqual(Math.Abs(aaa12), Math.Abs(a12), $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
+            Assert.LessOrEqual(Math.Abs(aaa21), Math.Abs(a21), $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
+        }
+
+        void TestGivensApproximationQuaternion(float a11, float a12, float a22)
+        {
+            MathUtils.ApproximateGivensRotationQuaternion(a11, a12, a22, out float x, out float w);
+            Assert.LessOrEqual(x, 1.0f);
+            Assert.GreaterOrEqual(x, -1.0f);
+            Assert.LessOrEqual(w, 1.0f);
+            Assert.GreaterOrEqual(w, -1.0f);
+            Assert.AreEqual(1.0f, x*x + w*w, ExpectedGivensEpsilon);
+
+            float a21 = a12;
+            Matrix4x4 m = new Matrix4x4(new Vector4(a11, a21, 0, 0), new Vector4(a12, a22, 0, 0), new Vector4(0, 0, 1, 0), new Vector4(0, 0, 0, 1));
+
+            Matrix4x4 qm = Matrix4x4.Rotate(new Quaternion(x, 0, 0, w));
+            Matrix4x4 rm = qm.transpose * m * qm;
+
+            Assert.LessOrEqual(Math.Abs(rm.m10), Math.Abs(a21), $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
+            Assert.LessOrEqual(Math.Abs(rm.m01), Math.Abs(a12), $"Givens rotation for (({a11}, {a12}), ({a21}, {a22})) does not diagonalize matrix");
         }
     }
 }
